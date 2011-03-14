90889f406c2a9469ac177d2a24ccd1a0aebd70aa
Alexandre Hamez <alexandre.hamez@gmail.com>
2011-03-14 21:33:09 +0100

    - Whitespaces

--- expat.c
+++ expat.c
@@ -3,16 +3,17 @@
 // Contributors:
 // - Alexandre Hamez <alexandre.hamez@gmail.com>
 //
-// This software is governed by the CeCILL-B license under French law and abiding
-// by the rules of distribution of free software. You can use, modify and/ or
-// redistribute the software under the terms of the CeCILL-B license as circulated
-// by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+// This software is governed by the CeCILL-B license under French law and
+// abiding by the rules of distribution of free software. You can use, modify
+// and/ or redistribute the software under the terms of the CeCILL-B license as
+// circulated by CEA, CNRS and INRIA at the following URL
+// "http://www.cecill.info".
 //
-// The fact that you are presently reading this means that you have had knowledge
-// of the CeCILL-B license and that you accept its terms.
+// The fact that you are presently reading this means that you have had
+// knowledge of the CeCILL-B license and that you accept its terms.
 //
-// You should have received a copy of the CeCILL-B license along with expatSML. See
-// the file LICENSE.
+// You should have received a copy of the CeCILL-B license along with expatSML.
+// See the file LICENSE.
 
 #include <expat.h>
 #include "SMLExpat.h"
